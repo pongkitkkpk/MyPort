8b3aafad3c146f75ba91e51aaaae3c52888d2d50
pongkitkkpk <pongkitkim@gmail.com>
2023-09-24 17:32:17 +0700

    Initial commit

---
+++ src/data/seo.js
@@ -0,0 +1,32 @@
+const SEO = [
+	{
+		page: "home",
+		description:
+			"I am a student at King Mongkut's University of Technology North Bangkok. I am proficient in logic and backend as my main focus. My extracurricular learning platform is YouTube. The channels I regularly study from are KongRuksiam Official",
+		keywords: ["Pongkit", "Pongkit Y", "PongkitYingyongtanasarn"],
+	},
+
+	{
+		page: "about",
+		description:
+			"I am a student at King Mongkut's University of Technology North Bangkok. I am proficient in logic and backend as my main focus. My extracurricular learning platform is YouTube. The channels I regularly study from are KongRuksiam Official",
+		keywords: ["Pongkit", "Pongkit Y", "PongkitYingyongtanasarn"],	
+	},
+
+
+	{
+		page: "projects",
+		description:
+			"I am a student at King Mongkut's University of Technology North Bangkok. I am proficient in logic and backend as my main focus. My extracurricular learning platform is YouTube. The channels I regularly study from are KongRuksiam Official",
+		keywords: ["Pongkit", "Pongkit Y", "PongkitYingyongtanasarn"],
+	},
+
+	{
+		page: "contact",
+		description:
+			"I am a student at King Mongkut's University of Technology North Bangkok. I am proficient in logic and backend as my main focus. My extracurricular learning platform is YouTube. The channels I regularly study from are KongRuksiam Official",
+		keywords: ["Pongkit", "Pongkit Y", "PongkitYingyongtanasarn"],
+	},
+];
+
+export default SEO;
---
+++ src/data/user.js
@@ -0,0 +1,72 @@
+const INFO = {
+	main: {
+		title: "Portfolio_Kim",
+		name: "Pongkit Y.",
+		email: "pongkitkim@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/pongkitkkpk",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://www.instagram.com/_kkpk__/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://www.facebook.com/kim.pongkit?locale=th_TH",
+	},
+
+	homepage: {
+		title: "Portfolio  Pongkit Yingyongtanasarn KMUTNB , Bangkok",
+		description:
+			"I am a student at King Mongkut's University of Technology North Bangkok. I am proficient in logic and backend as my main focus. My extracurricular learning platform is YouTube. The channels I regularly study from are KongRuksiam Official",
+	},
+
+	about: {
+		title: "Hi I’m Pongkit Y. ",
+		description:
+			"I have some of my learning content on GitHub, and there are parts that are project-based while studying. What I'm particularly proud of is the CarbonX project. It's a project I actually worked on to compete outside of the university, and I coded it using HTML, CSS, PHP, and JavaScript.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		
+
+		
+
+		{
+			title: "webboard-kakak",
+			description:
+				"This is my first learning project where I studied HTML, CSS, PHP, and connecting to a database.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/pongkitkkpk/webboard-kakak",
+		},
+
+		{
+			title: "CarbonX",
+			description:
+				"This is my first project that I did with my partner to compete externally. Sometimes, this project will use only simple things like HTML, CSS, PHP, and a database using PHPMyAdmin.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com/pongkitkkpk/carbonX",
+		},
+
+		{
+			title: "Network Programming",
+			description:
+				"And this is the Network Programming course, which will focus on threads and how to make the server and client communicate with each other.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/java/java.png",
+			linkText: "View Project",
+			link: "https://github.com/pongkitkkpk/Wireless_Network_DCK",
+		},
+
+		
+	],
+};
+
+export default INFO;
